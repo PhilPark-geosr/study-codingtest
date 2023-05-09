90834532d0b15c69d8d9852ec1d992cfb0a081aa
PhilPark-geosr <parkpill26@gmail.com>
2023-05-09 11:24:47 +0900

    add week4 Reverse Linked List

---
+++ week4/206. Reverse Linked List.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head
+        prev = None
+        while current:
+            next = current.next
+            current.next = prev
+            prev, current = current, next
+        return prev
